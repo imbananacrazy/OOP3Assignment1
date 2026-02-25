1b9eb9a9f69bd59bf7a23122747cad7174952dee
Jaafar <jaafaradegoke@gmail.com>
2026-02-24 22:27:38 -0700

    selection sort added

--- src/utilities/SortUtility.java
+++ src/utilities/SortUtility.java
@@ -29,15 +29,39 @@ public class SortUtility
 	}
     
 
-	public static void selectionSort(Shape[] shapes, Comparator<Shape> comparator)
-	{
-		
-	}
+	public static void selectionSort(Shape[] arr, Comparator<Shape> comp) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int maxIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (comp.compare(arr[j], arr[maxIndex]) > 0) {
+                    maxIndex = j;
+                }
+            }
+
+            swap(arr, i, maxIndex);
+        }
+    }
 	
-	public static void insertionSort(Shape[] shapes, Comparator<Shape> comparator)
-	{
-		
-	}
+	public static void insertionSort(Shape[] arr, Comparator<Shape> comp) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+
+            Shape key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && comp.compare(arr[j], key) < 0) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
 	
 	public static void mergeSort(Shape[] shapes, Comparator<Shape> comparator)
 	{
