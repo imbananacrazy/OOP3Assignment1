23ff8b547dcf137134ca01fc7ddd488dd1f9d815
imbananacrazy <bananacrazyness21@gmail.com>
2026-02-26 07:47:27 -0700

    heap sort

--- src/utilities/SortUtility.java
+++ src/utilities/SortUtility.java
@@ -29,32 +29,37 @@ public class SortUtility
 	}
     
 
-	public static void selectionSort(Shape[] arr, Comparator<Shape> comp) {
-        int n = arr.length;
+	public static void selectionSort(Shape[] arr, Comparator<Shape> comp) 
+	{
+	    int n = arr.length;
 
-        for (int i = 0; i < n - 1; i++) {
+	    for (int i = 0; i < n - 1; i++) {
 
-            int maxIndex = i;
+	        int maxIndex = i;
 
-            for (int j = i + 1; j < n; j++) {
-                if (comp.compare(arr[j], arr[maxIndex]) > 0) {
-                    maxIndex = j;
-                }
-            }
+	        for (int j = i + 1; j < n; j++) 
+	        {
+	            if (comp.compare(arr[j], arr[maxIndex]) > 0) maxIndex = j;
+	        }
 
-            swap(arr, i, maxIndex);
-        }
-    }
+	        Shape temp = arr[i];
+	        arr[i] = arr[maxIndex];
+	        arr[maxIndex] = temp;
+	    }
+	}
 	
-	public static void insertionSort(Shape[] arr, Comparator<Shape> comp) {
+	public static void insertionSort(Shape[] arr, Comparator<Shape> comp) 
+	{
         int n = arr.length;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) 
+        {
 
             Shape key = arr[i];
             int j = i - 1;
 
-            while (j >= 0 && comp.compare(arr[j], key) < 0) {
+            while (j >= 0 && comp.compare(arr[j], key) < 0) 
+            {
                 arr[j + 1] = arr[j];
                 j--;
             }
@@ -73,8 +78,41 @@ public class SortUtility
 		
 	}
 	
+	//edited gfg
 	public static void heapSort(Shape[] shapes, Comparator<Shape> comparator)
 	{
-		
+	    int n = shapes.length;
+
+	    for (int i = n / 2 - 1; i >= 0; i--) heapify(shapes, n, i, comparator);
+
+	    for (int i = n - 1; i > 0; i--) {
+
+	        Shape temp = shapes[0];
+	        shapes[0] = shapes[i];
+	        shapes[i] = temp;
+
+	        heapify(shapes, i, 0, comparator);
+	    }
+	}
+
+	static void heapify(Shape[] arr, int n, int i, Comparator<Shape> comparator)
+	{
+	    int smallest = i;
+
+	    int left = 2 * i + 1;
+	    int right = 2 * i + 2;
+
+	    if (left < n && comparator.compare(arr[left], arr[smallest]) < 0) smallest = left;
+	    if (right < n && comparator.compare(arr[right], arr[smallest]) < 0) smallest = right;
+
+	    if (smallest != i) 
+	    {
+
+	        Shape temp = arr[i];
+	        arr[i] = arr[smallest];
+	        arr[smallest] = temp;
+
+	        heapify(arr, n, smallest, comparator);
+	    }
 	}
 }
